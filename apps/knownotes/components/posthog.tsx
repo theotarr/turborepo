4ca2afb7e404a316dd639ad38a03ab11e2050f12
Theo Tarr <theodore@tarr.com>
2024-10-05 13:58:09 -0400

    Update env

--- apps/knownotes/components/posthog.tsx
+++ apps/knownotes/components/posthog.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import { env } from "@/env";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
 }
--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -14,7 +14,6 @@ export const env = createEnv({
   },
   client: {},
   experimental__runtimeEnv: {},
-
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
 });
