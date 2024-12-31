4a20d32a63be583ffb6fc1632fc621d664541c9c
Theo Tarr <theodore@tarr.com>
2024-12-31 15:07:31 -0500

    Set up posthog and session recording

--- apps/levely/src/app/_layout.tsx
+++ apps/levely/src/app/_layout.tsx
@@ -12,7 +12,7 @@ import { TRPCProvider } from "~/utils/api";
 import "../globals.css";
 
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { PostHogProvider } from "posthog-react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -27,14 +27,39 @@ const LIGHT_THEME: Theme = {
 export default function RootLayout() {
   return (
     <TRPCProvider>
-      <GestureHandlerRootView>
-        <BottomSheetModalProvider>
+      <PostHogProvider
+        apiKey="phc_UUGaxECpebe607OnNeXeFhvHnPLiF4k4VYxMBi1O6v2"
+        options={{
+          host: "https://us.i.posthog.com",
+          enableSessionReplay: true,
+          sessionReplayConfig: {
+            // Whether text inputs are masked. Default is true.
+            // Password inputs are always masked regardless
+            maskAllTextInputs: true,
+            // Whether images are masked. Default is true.
+            maskAllImages: true,
+            // Capture logs automatically. Default is true.
+            // Android only (Native Logcat only)
+            captureLog: true,
+            // Whether network requests are captured in recordings. Default is true
+            // Only metric-like data like speed, size, and response code are captured.
+            // No data is captured from the request or response body.
+            // iOS only
+            captureNetworkTelemetry: true,
+            // Deboucer delay used to reduce the number of snapshots captured and reduce performance impact. Default is 500ms
+            androidDebouncerDelayMs: 500,
+            // Deboucer delay used to reduce the number of snapshots captured and reduce performance impact. Default is 1000ms
+            iOSdebouncerDelayMs: 1000,
+          },
+        }}
+      >
+        <GestureHandlerRootView>
           <ThemeProvider value={LIGHT_THEME}>
             <StatusBar style="light" />
             <Stack />
           </ThemeProvider>
-        </BottomSheetModalProvider>
-      </GestureHandlerRootView>
+        </GestureHandlerRootView>
+      </PostHogProvider>
     </TRPCProvider>
   );
 }
