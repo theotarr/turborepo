4785e834ab7b3cefb9aef80eea232385460f7b34
Theo Tarr <theodore@tarr.com>
2025-01-01 08:57:32 -0500

    Splash screen and redirect to correct page on load

--- apps/levely/src/app/(onboarding)/_layout.tsx
+++ apps/levely/src/app/(onboarding)/_layout.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
@@ -6,13 +6,27 @@ import { getOnboardingComplete } from "~/lib/storage";
 
 export default function OnboardingLayout() {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     void (async () => {
       const hasOnboarded = await getOnboardingComplete();
       if (hasOnboarded) router.replace("/dashboard");
+      setIsLoading(false);
     })();
   }, [router]);
+
+  if (isLoading)
+    return (
+      <View className="h-full flex-1 bg-background">
+        <Stack.Screen
+          options={{
+            title: "Onboarding",
+            header: () => <></>,
+          }}
+        />
+      </View>
+    );
 
   return (
     <View className="h-full flex-1">
