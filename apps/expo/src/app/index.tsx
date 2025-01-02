b9d769fcd700cdb192ed5a75bf3c0da8faa3ae0c
Theo Tarr <theodore@tarr.com>
2025-01-02 11:56:47 -0500

    Fix onboarding styles

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -24,12 +24,10 @@ export default function Page() {
   const createMobileUser = api.auth.createMobileUser.useMutation();
 
   if (user) {
-    // void AsyncStorage.getItem("onboardingComplete").then((value) => {
-    //   if (value === "true") router.replace("/(dashboard)/dashboard");
-    // });
-    // return <Redirect href={"/onboarding"} />;
-    // Skip onboarding for now
-    return <Redirect href={"/dashboard"} />;
+    void AsyncStorage.getItem("onboardingComplete").then((value) => {
+      if (value === "true") router.replace("/(dashboard)/dashboard");
+    });
+    return <Redirect href={"/onboarding"} />;
   }
 
   return (
