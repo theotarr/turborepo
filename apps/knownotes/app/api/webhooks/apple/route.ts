6fcacebd0e7717ae8111be199334c7ccf570c594
Theo Tarr <theodore@tarr.com>
2025-01-14 16:22:25 -0500

    Log body

--- apps/knownotes/app/api/webhooks/apple/route.ts
+++ apps/knownotes/app/api/webhooks/apple/route.ts
@@ -5,6 +5,7 @@ const SUPERWALL_WEBHOOK_URL =
 
 export async function POST(req: Request) {
   const body = await req.json();
+  console.log("Body", body);
 
   // Forward the event to Superwall.
   const superwallResponse = await fetch(SUPERWALL_WEBHOOK_URL, {
