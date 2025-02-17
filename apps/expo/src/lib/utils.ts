eab5ca9972148273500c43f33a69b2ad4ffab249
Theo Tarr <theodore@tarr.com>
2025-02-17 11:00:20 -0500

    Capitalize utils

--- apps/expo/src/lib/utils.ts
+++ apps/expo/src/lib/utils.ts
@@ -29,3 +29,14 @@ export function formatLectureType(type: string) {
     .replace("_", " ")
     .replace(/\b\w/g, (c) => c.toUpperCase());
 }
+
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function capitalizeWords(str: string) {
+  return str
+    .split(" ")
+    .map((word) => capitalize(word))
+    .join(" ");
+}
