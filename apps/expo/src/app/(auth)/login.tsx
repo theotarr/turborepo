3a0a01bc731381679200f2fe324071eb8176efe3
Theo Tarr <theodore@tarr.com>
2025-02-17 18:10:20 -0500

    Update sign in pages

--- apps/expo/src/app/(auth)/login.tsx
+++ apps/expo/src/app/(auth)/login.tsx
@@ -1,36 +1,26 @@
 import { View } from "react-native";
-import appsFlyer from "react-native-appsflyer";
 import { SafeAreaView } from "react-native-safe-area-context";
-import * as AppleAuthentication from "expo-apple-authentication";
 import { Stack } from "expo-router";
 import { Aperture } from "lucide-react-native";
 
+import { AuthForm } from "~/components/auth-form";
 import { TrustPilot } from "~/components/trust-pilot";
-import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
 import { NAV_THEME } from "~/lib/constants";
 import { useColorScheme } from "~/lib/theme";
-import { api } from "~/utils/api";
-import { useSignIn } from "~/utils/auth";
-import { setToken } from "~/utils/session-store";
 
 export default function Page() {
-  const utils = api.useUtils();
   const { colorScheme } = useColorScheme();
-
-  const signIn = useSignIn();
-  const createMobileUser = api.auth.createMobileUser.useMutation();
 
   return (
     <SafeAreaView className="bg-background">
       <Stack.Screen
         options={{
-          headerLeft: () => <></>,
-          title: "",
+          headerShown: false,
         }}
       />
       <View className="flex h-full w-full flex-col justify-between p-4">
-        <View className="flex flex-col items-center gap-4">
+        <View className="mt-16 flex flex-col items-center gap-4">
           <View className="mb-2 flex flex-row items-center gap-2">
             <Aperture
               size={20}
@@ -43,68 +33,9 @@ export default function Page() {
           <Text className="text-center text-5xl font-bold tracking-tighter text-secondary-foreground">
             The AI Assistant{"\n"} For Students
           </Text>
-          <View className="mt-4">
-            <TrustPilot />
-          </View>
+          <TrustPilot />
         </View>
-        <View className="w-full gap-2">
-          <AppleAuthentication.AppleAuthenticationButton
-            buttonType={
-              AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN
-            }
-            buttonStyle={
-              colorScheme === "light"
-                ? AppleAuthentication.AppleAuthenticationButtonStyle.BLACK
-                : AppleAuthentication.AppleAuthenticationButtonStyle.WHITE
-            }
-            cornerRadius={100}
-            style={{
-              width: "100%",
-              height: 56,
-            }}
-            onPress={async () => {
-              const credential = await AppleAuthentication.signInAsync({
-                requestedScopes: [
-                  AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-                  AppleAuthentication.AppleAuthenticationScope.EMAIL,
-                ],
-              });
-
-              // Signed in. Check to see if the user is new or existing and handle accordingly.
-              const { user: appStoreUserId, email, fullName } = credential;
-              const name = fullName
-                ? `${fullName.givenName} ${fullName.familyName}`
-                : undefined;
-              await createMobileUser.mutateAsync({
-                appStoreUserId,
-                email: email ?? undefined,
-                name: name ?? undefined,
-              });
-
-              // Store the app store user ID as then token prefixed with apple_. Then refetch the session to redirect.
-              setToken(`apple_${appStoreUserId}`);
-              await appsFlyer.logEvent("af_login", {});
-              await utils.auth.getSession.invalidate();
-            }}
-          />
-          <Button
-            variant="outline"
-            className="flex w-full flex-row gap-2 rounded-full"
-            size="lg"
-            onPress={async () => {
-              await signIn();
-              await appsFlyer.logEvent("af_login", {});
-            }}
-          >
-            <Aperture
-              size={20}
-              color={NAV_THEME[colorScheme].secondaryForeground}
-            />
-            <Text className="native:text-xl text-xl">
-              Continue on KnowNotes.ai
-            </Text>
-          </Button>
-        </View>
+        <AuthForm />
       </View>
     </SafeAreaView>
   );
--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,30 +1,23 @@
 import { useEffect } from "react";
 import { View } from "react-native";
-import appsFlyer from "react-native-appsflyer";
 import { SafeAreaView } from "react-native-safe-area-context";
-import * as AppleAuthentication from "expo-apple-authentication";
 import { Stack, useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Superwall from "@superwall/react-native-superwall";
 import { Aperture } from "lucide-react-native";
 
+import { AuthForm } from "~/components/auth-form";
 import { TrustPilot } from "~/components/trust-pilot";
-import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
 import { NAV_THEME } from "~/lib/constants";
 import { useColorScheme } from "~/lib/theme";
 import { api } from "~/utils/api";
-import { useSignIn } from "~/utils/auth";
-import { setToken } from "~/utils/session-store";
 
 export default function Page() {
-  const utils = api.useUtils();
   const router = useRouter();
   const { colorScheme } = useColorScheme();
 
-  const signIn = useSignIn();
   const { isLoading, data: session } = api.auth.getSession.useQuery();
-  const createMobileUser = api.auth.createMobileUser.useMutation();
 
   useEffect(() => {
     const identifyUser = async () => {
@@ -39,19 +32,19 @@ export default function Page() {
     if (session) {
       void identifyUser();
       void AsyncStorage.getItem("onboardingComplete").then((value) => {
-        if (value === "false") router.replace("/(dashboard)/dashboard");
+        if (value === "true") router.replace("/(dashboard)/dashboard");
         else router.replace("/onboarding");
       });
     }
   }, [router, session]);
 
-  if (isLoading || session) return <Stack.Screen options={{ title: "" }} />;
+  if (isLoading) return <Stack.Screen options={{ title: "" }} />;
 
   return (
     <SafeAreaView className="bg-background">
-      <Stack.Screen options={{ title: "" }} />
-      <View className="flex h-full w-full flex-col justify-between p-4">
-        <View className="flex flex-col items-center gap-4">
+      <Stack.Screen options={{ headerShown: false }} />
+      <View className="flex h-full w-full flex-col p-4">
+        <View className="mt-16 flex flex-col items-center gap-4">
           <View className="mb-2 flex flex-row items-center gap-2">
             <Aperture
               size={20}
@@ -64,72 +57,9 @@ export default function Page() {
           <Text className="text-center text-5xl font-bold tracking-tighter text-secondary-foreground">
             The AI Assistant{"\n"} For Students
           </Text>
-          <View className="mt-4">
-            <TrustPilot />
-          </View>
+          <TrustPilot />
         </View>
-        <View className="w-full gap-2">
-          <AppleAuthentication.AppleAuthenticationButton
-            buttonType={
-              AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN
-            }
-            buttonStyle={
-              colorScheme === "light"
-                ? AppleAuthentication.AppleAuthenticationButtonStyle.BLACK
-                : AppleAuthentication.AppleAuthenticationButtonStyle.WHITE
-            }
-            cornerRadius={100}
-            style={{
-              width: "100%",
-              height: 56,
-            }}
-            onPress={async () => {
-              const credential = await AppleAuthentication.signInAsync({
-                requestedScopes: [
-                  AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-                  AppleAuthentication.AppleAuthenticationScope.EMAIL,
-                ],
-              });
-
-              // Signed in. Check to see if the user is new or existing and handle accordingly.
-              const { user: appStoreUserId, email, fullName } = credential;
-              const name = fullName
-                ? `${fullName.givenName} ${fullName.familyName}`
-                : undefined;
-              await createMobileUser.mutateAsync({
-                appStoreUserId,
-                email: email ?? undefined,
-                name: name ?? undefined,
-              });
-
-              // Store the app store user ID as then token prefixed with apple_. Then refetch the session to redirect.
-              setToken(`apple_${appStoreUserId}`);
-              await appsFlyer.logEvent("af_login", {
-                af_registration_method: "apple",
-              });
-              await utils.auth.getSession.invalidate();
-            }}
-          />
-          <Button
-            variant="outline"
-            className="flex w-full flex-row gap-2 rounded-full"
-            size="lg"
-            onPress={async () => {
-              await signIn();
-              await appsFlyer.logEvent("af_login", {
-                af_registration_method: "web",
-              });
-            }}
-          >
-            <Aperture
-              size={20}
-              color={NAV_THEME[colorScheme].secondaryForeground}
-            />
-            <Text className="native:text-xl text-xl">
-              Continue on KnowNotes.ai
-            </Text>
-          </Button>
-        </View>
+        <AuthForm className="mt-auto" />
       </View>
     </SafeAreaView>
   );
