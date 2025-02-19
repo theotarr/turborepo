1d0525c5e475e749616eea02fd5001e1155f70a9
Theo Tarr <theodore@tarr.com>
2025-02-19 17:45:11 -0500

    Remove back buttom from login to prevent mobile sign in from browsing

--- apps/knownotes/app/(auth)/login/page.tsx
+++ apps/knownotes/app/(auth)/login/page.tsx
@@ -38,12 +38,9 @@ export const metadata = {
   },
 };
 export default async function LoginPage() {
-  // const session = await auth();
-  // if (session) redirect("/dashboard");
-
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <Link
+      {/* <Link
         href="/"
         className={cn(
           buttonVariants({ variant: "ghost" }),
@@ -54,7 +51,7 @@ export default async function LoginPage() {
           <Icons.chevronLeft className="mr-2 h-4 w-4" />
           Back
         </>
-      </Link>
+      </Link> */}
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <Icons.logo className="mx-auto h-6 w-6" />
--- apps/knownotes/app/(auth)/mobile/page.tsx
+++ apps/knownotes/app/(auth)/mobile/page.tsx
@@ -7,6 +7,7 @@ import { auth } from "@acme/auth";
 export default async function MobileLoginSuccessPage() {
   const session = await auth();
   if (!session) return null;
+
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
