c09821bcf29fe981cc48827998fb8c36f90fd8bb
Theo Tarr <theodore@tarr.com>
2025-04-01 21:04:33 -0400

    Refactor LectureShareLayout to enhance UI with logo, updated text, and improved link structure

--- apps/knownotes/app/(share)/share/layout.tsx
+++ apps/knownotes/app/(share)/share/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Icons } from "@/components/icons";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
@@ -11,20 +12,29 @@ export default async function LectureShareLayout({
 }: LectureShareLayoutProps) {
   return (
     <>
-      <div className="fixed top-0 z-10 flex w-screen items-center gap-x-6 bg-secondary px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-        <p className="text-sm leading-6 text-secondary-foreground">
-          Notes written with KnowNotes AI
-        </p>
-        <Link
-          href="/register"
-          className={cn(
-            buttonVariants({ size: "sm" }),
-            "h-auto w-auto rounded-full px-2.5 py-1",
-          )}
-        >
-          Try now
-        </Link>
-        <div className="flex flex-1 justify-end"></div>
+      <div className="fixed top-0 z-10 flex w-full items-center justify-between gap-x-6 bg-secondary p-3.5">
+        <div className="flex items-center space-x-6">
+          <Link href="/?ref=share" className="flex items-center space-x-2">
+            <Icons.logo className="h-6 w-6" />
+            <span className="font-semibold tracking-tight text-secondary-foreground">
+              KnowNotes
+            </span>
+          </Link>
+          <p className="text-sm font-medium leading-6 text-muted-foreground">
+            The AI assistant for stressed students
+          </p>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <Link
+            href="/register?ref=share"
+            className={cn(
+              buttonVariants({ size: "sm" }),
+              "h-auto w-auto rounded-full px-2.5 py-1",
+            )}
+          >
+            Try KnowNotes Free
+          </Link>
+        </div>
       </div>
       <main className="mt-16 flex w-full flex-1 flex-col overflow-hidden">
         {children}
