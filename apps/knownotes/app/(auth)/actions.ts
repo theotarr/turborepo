44f9b68ff6365a6da398a61b14829585d9949c33
Theo Tarr <theodore@tarr.com>
2025-04-28 16:23:05 -0400

    Implement course deletion and update functionality, refactor course creation parameter

--- apps/knownotes/app/(auth)/actions.ts
+++ apps/knownotes/app/(auth)/actions.ts
@@ -5,14 +5,41 @@ import { redirect } from "next/navigation";
 import { auth } from "@acme/auth";
 import { db } from "@acme/db";
 
-export async function createCourse(courseName: string) {
+export async function createCourse(name: string) {
   const session = await auth();
   if (!session) return redirect("/login");
 
   return await db.course.create({
     data: {
-      name: courseName,
+      name,
       userId: session.user.id,
+    },
+  });
+}
+
+export async function deleteCourse(courseId: string) {
+  const session = await auth();
+  if (!session) return redirect("/login");
+
+  return await db.course.delete({
+    where: {
+      id: courseId,
+      userId: session.user.id,
+    },
+  });
+}
+
+export async function updateCourse(courseId: string, name: string) {
+  const session = await auth();
+  if (!session) return redirect("/login");
+
+  return await db.course.update({
+    where: {
+      id: courseId,
+      userId: session.user.id,
+    },
+    data: {
+      name,
     },
   });
 }
