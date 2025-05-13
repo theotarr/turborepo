f9092126ab22afcd19b1cef1f5a5f0a00c32b7ca
Theo Tarr <theodore@tarr.com>
2025-05-13 19:24:47 -0400

    Update styles

--- apps/knownotes/app/(dashboard)/dashboard/page.tsx
+++ apps/knownotes/app/(dashboard)/dashboard/page.tsx
@@ -62,71 +62,76 @@ export default async function DashboardPage() {
   });
 
   return (
-    <div className="mx-auto max-w-3xl space-y-12 pt-16">
-      <h1 className="text-center text-3xl font-semibold tracking-tight">
-        What do you want to learn today?
-      </h1>
-
-      <div className="grid auto-rows-fr grid-cols-1 gap-4 sm:grid-cols-3">
-        <QuickActions />
-      </div>
-
-      {/* Courses section */}
-      <div className="space-y-4">
-        <div className="flex items-center justify-between">
-          <h2 className="text-lg font-medium">Courses</h2>
-          <CourseCreateDialog
-            className={buttonVariants({
-              variant: "outline",
-              size: "sm",
-            })}
-          />
+    <div className="mx-auto max-w-3xl flex-col space-y-12 pt-24 lg:pt-32">
+      <div className="flex flex-col gap-4">
+        <h1 className="font-heading text-pretty text-center text-[29px] font-semibold tracking-tighter sm:text-[32px] md:text-[46px]">
+          What do you want to learn?
+        </h1>
+        <div className="grid auto-rows-fr grid-cols-1 gap-4 sm:grid-cols-3">
+          <QuickActions />
         </div>
 
-        {user?.courses.length! > 0 ? (
-          <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
-            {user?.courses.map((course) => (
-              <CourseCard className="w-full" key={course.id} course={course} />
-            ))}
-          </div>
-        ) : (
-          <EmptyPlaceholder>
-            <EmptyPlaceholder.Icon name="folder" />
-            <EmptyPlaceholder.Title>
-              Add a course to get started
-            </EmptyPlaceholder.Title>
-            <EmptyPlaceholder.Description>
-              You can group your lectures and notes by course, click here to add
-              one.
-            </EmptyPlaceholder.Description>
+        {/* Courses section */}
+        <div className="space-y-4">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-medium">Courses</h2>
             <CourseCreateDialog
               className={buttonVariants({
-                variant: "default",
+                variant: "outline",
                 size: "sm",
               })}
             />
-          </EmptyPlaceholder>
-        )}
-      </div>
+          </div>
 
-      {/* Recent notes section */}
-      <div className="space-y-4">
-        <div className="flex items-center justify-between">
-          <h2 className="text-lg font-medium">Recent</h2>
-          <Link
-            href="/library?tab=notes"
-            className="text-sm font-medium text-secondary-foreground hover:underline"
-          >
-            View all
-          </Link>
+          {user?.courses.length! > 0 ? (
+            <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
+              {user?.courses.map((course) => (
+                <CourseCard
+                  className="w-full"
+                  key={course.id}
+                  course={course}
+                />
+              ))}
+            </div>
+          ) : (
+            <EmptyPlaceholder>
+              <EmptyPlaceholder.Icon name="folder" />
+              <EmptyPlaceholder.Title>
+                Add a course to get started
+              </EmptyPlaceholder.Title>
+              <EmptyPlaceholder.Description>
+                You can group your lectures and notes by course, click here to
+                add one.
+              </EmptyPlaceholder.Description>
+              <CourseCreateDialog
+                className={buttonVariants({
+                  variant: "default",
+                  size: "sm",
+                })}
+              />
+            </EmptyPlaceholder>
+          )}
         </div>
-        <LectureSearch courses={user?.courses} />
-      </div>
 
-      <LectureCreateActions
-        userId={user?.id as string}
-        courses={user?.courses as Course[]}
-      />
+        {/* Recent notes section */}
+        <div className="space-y-4">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-medium">Recent</h2>
+            <Link
+              href="/library?tab=notes"
+              className="text-sm font-medium text-secondary-foreground hover:underline"
+            >
+              View all
+            </Link>
+          </div>
+          <LectureSearch courses={user?.courses} />
+        </div>
+
+        <LectureCreateActions
+          userId={user?.id as string}
+          courses={user?.courses as Course[]}
+        />
+      </div>
     </div>
   );
 }
